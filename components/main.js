18739f6b324ce052720fc82ce17d0b68b8b43418
Richard Huang <richardhuang.huang@gmail.com>
2016-12-28 18:57:16 -0800

    updated main.js

--- components/main.js
+++ components/main.js
@@ -2,9 +2,11 @@ function compile(){
 	var html=document.getElementById("html");
 	var css=document.getElementById("css");
 	var js=document.getElementById("js");
+	var result=document.getElementById("result").contentWindow.document;
 	document.body.onkeydown=function(){
+		console.log("hello")
 		result.open();
-		result.write();
+		result.writeln(html.value);
 		result.close();
 	}
 }
