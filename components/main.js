3c74df9d3a966ae7bf59f99802c8933b1a800690
Richard Huang <richardhuang.huang@gmail.com>
2016-12-29 18:06:42 -0800

    fixed one key lag

--- components/main.js
+++ components/main.js
@@ -3,7 +3,7 @@ function compile(){
 	var css=document.getElementById("css");
 	var js=document.getElementById("js");
 	var result=document.getElementById("result").contentWindow.document;
-	document.body.onkeydown=function(){
+	document.body.onkeyup=function(){
 		result.open();
 		result.writeln(html.value+"<style>"+css.value+"</script>"+"<script>"+js.value+"</script>");
 		result.close();
